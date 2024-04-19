5887ae1031e4a9287f0249e96765ebed4ada1479
Laira Dumaran <lairaannm@gmail.com>
2024-04-19 19:52:10 +0800

    added three lines for ptc

--- Prog1.java
+++ Prog1.java
@@ -1,5 +1,7 @@
 Public class Prog1 {
        public static void main (String [] args ) {
-       System.out.println ("Pateros Technological College");
+       System.out.println ("Pateros");
+	System.out.println ("Technological ");
+	System.out.println ("College");
    }
 }
